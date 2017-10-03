d010ad8faed79fad76286d721e0db3f73adf5369
Heng <lhengi888@gmail.com>
2017-10-02 22:02:20 -0500

    lab3 fixed

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,43 +1,5 @@
-/*
-  LiquidCrystal Library - scrollDisplayLeft() and scrollDisplayRight()
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD and uses the
- scrollDisplayLeft() and scrollDisplayRight() methods to scroll
- the text.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalScroll
-
-*/
+//Heng Li
+//hli212
 
 // include the library code:
 #include <LiquidCrystal.h>
@@ -49,9 +11,63 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  
 }
 
-void loop() {
+void loop()
+{
+  // scroll 13 positions (string length) to the left
+  // to move it offscreen left:
+  
+  String quote = "abcdefghijklmnopqrstuvwx";
+  int n = quote.length();
+  /*
+  for (int positionCounter = 31; positionCounter >= 0; positionCounter--) {
+    // scroll one position left:
+    lcd.setCursor(0,0);
+    lcd.print("Heng Li");
+    
+    lcd.setCursor(positionCounter,1);
+    lcd.print(quote);
+    String quote2 = &quote[n -1- positionCounter];
+    lcd.setCursor(0,1);
+    lcd.print(quote2);
+    delay(500);
+    lcd.clear();
+    // wait a bit:
+    
+  }
+  */
+
+  for(int i = 15; i >= 0;i--)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Heng Li");
+    
+    lcd.setCursor(i,1);
+    lcd.print(quote);
+    delay(200);
+    lcd.clear();
+  }
+
+  for(int i = 1 ; i < n; i++)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Heng Li");
+    
+    String quote1 = &quote[i];
+    lcd.setCursor(0,1);
+    lcd.print(quote1);
+    delay(200);
+    lcd.clear();
+  }
+
+  // delay at the end of the full loop:
+  //delay(1000);
+
 
 }
+
+
+
+
